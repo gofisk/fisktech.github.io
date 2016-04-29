0c436eb56f6fe3985f87f150f7d302398f149061
gofisk <ehstechdept.scott@gmail.com>
2016-04-29 10:43:04 -0500

    Google JSON Test

--- dev/client/ehs/library/assets/data/resources.js
+++ dev/client/ehs/library/assets/data/resources.js
@@ -1,21 +1,49 @@
-var sgResources = angular.module('sgResources', ['ui.unique']);
-sgResources.controller('sgController', [
+/* Copyright (C) Scott Benjamin Fisk - All Rights Reserved
+ * Unauthorized copying of this file via any medium is strictly prohibited
+ * This file may contain proprietary code and/or confidential information
+ * Written by Scott Benjamin Fisk <dev@gofisk.com>, April 2016
+ */
+
+angular.module('ehsResources', ['ui.unique']).controller('ehsController', [
     '$scope',
     '$http',
     function ($scope, $http) {
-        $scope.fs_dir = {
-            'sgResources': {
-                'directory': [
-                    {
-                        'ID': '1',
-                        'TITLE': 'Test Data',
-                        'DESCRIPTION': 'Test Description',
-                        'LINK': '#',
-                        'CATEGORY': 'Books'
-                    }
-                ]
+        //UNIQUE SPREADSHEET KEY - FOUND IN SPREADHSEET URL
+        var key = '1VsJKUv0uoo4GtIOMKPOaaRijXyiFQMJdt091GEf3nQg';
+        
+        //DO NOT CHANGE THIS URL PATTERN - IT RETURNS SPREADSHEET DATA IN JSON FORMAT
+        var url = 'https://spreadsheets.google.com/feeds/list/'+key+'/od6/public/values?alt=json';
+        
+         //PARSE JSON DATA FROM SPREADSHEET - VAR NAMES MUST MATCH TOP ROW OF SPREADSHEET
+        var parse = function(entry) {
+            console.log(entry);
+            var title = entry['gsx$title']['$t'];
+            var category = entry['gsx$category']['$t'];
+            var description = entry['gsx$description']['$t'];
+            var link = entry['gsx$link']['$t'];
+            return {
+                title: title, category: category, description: description, link: link
+            };
+        }
+    
+        $scope.loading = true;
+        
+        //PUSH PARSED DATA INTO AN ARRAY
+        $http.get(url)
+        .success(function(response) {
+            var entries = response['feed']['entry'];
+            $scope.ehsData = [];
+            for (key in entries) {
+                var content = entries[key];
+                $scope.ehsData.push(parse(content));
             }
-        };
+        })
+    
+        .finally(function () {
+        // Hide loading spinner whether our call succeeded or failed.
+        $scope.loading = false;
+        });
+    
         $scope.clearFilter = function () {
             $scope.search = {};
         };
