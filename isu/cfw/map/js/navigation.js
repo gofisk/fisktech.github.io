5b20a9de762854fcc874369094f5d58333aa48d3
Scott Fisk <github@scottfisk.org>
2018-10-15 16:29:25 -0400

    New Landing Page

--- isu/cfw/map/js/navigation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".ASB").click(function () {
-        map.setView([42.03469, -93.64564], 20)
-        L.marker([42.03469, -93.64564], {
-            icon: transmarker
-        }).addTo(map).bindPopup(ASB_pop /*,(autopad)*/ ).openPopup();
-    });
-})
--- isu/cfw/map/js/panorama.js
+++
@@ -1,9 +0,0 @@
-pannellum.viewer('panorama', {
-    "type": "equirectangular",
-    "panorama": "https://gofisk.com/isu/cfw/map/images/360/WEST_LAWN.jpg",
-    "haov": 360,
-    "vaov": 50,
-    "vOffset": 0,
-    "yaw": 181,
-    "title": "West Lawn"
-});
